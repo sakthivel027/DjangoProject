4c6f8ef36b2ccd2658e6062678e789a791a74d32
Sakthivel <73012840+sakthivel027@users.noreply.github.com>
2025-01-31 19:43:41 +0530

    Create manage.py
    added manage.py

---
+++ manage.py
@@ -0,0 +1,20 @@
+manage.py
+
+#test = [1, 2, 3, 4,9,9,9,9,9,9,2, 2, 3, 1, 4, 4, 4]
+#count = 0
+#for i in range(len(test)):
+#    for k in range(len(test)):
+#        if i == k:
+#            print('print')   #continue
+#            print(k)
+#            print(len(k))
+#        elif (i == k):       #test[i] == test[k]:
+#            k.append('0')    #del test[k]     
+#            count += i       #test.append('0') 
+#        else:                #count = count + 1 
+#            print('Nothing')
+
+
+
+
+
